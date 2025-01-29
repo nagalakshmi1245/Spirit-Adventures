544d361242a5d610cb6b8d6b5f163aa5a4858914
Anjanee Devi <143275249+AnjaneeDevi-Gundala@users.noreply.github.com>
2025-01-29 10:36:55 +0530

    Update models.py

--- spiritproject/app1/models.py
+++ spiritproject/app1/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class category(models.Model):
+    name=models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
+
+class sub_category(models.Model):
+    name=models.CharField(max_length=100)
+    cid = models.ForeignKey(
+        category, 
+        on_delete=models.CASCADE, 
+        blank=False,
+        related_name='subcategories'
+    )
+    def __str__(self):
+        return self.name
