f1ded381fd0d77144ab018ce2a2adab4168acf0e
Anjanee Devi <143275249+AnjaneeDevi-Gundala@users.noreply.github.com>
2025-01-29 10:37:24 +0530

    Update admin.py

--- spiritproject/app1/admin.py
+++ spiritproject/app1/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import category,sub_category
 # Register your models here.
+class categoryAdmin(admin.ModelAdmin):
+    list_display=['name']
+class sub_categoryAdmin(admin.ModelAdmin):
+    list_display=['name']
+
+admin.site.register(category,categoryAdmin)
+admin.site.register(sub_category,sub_categoryAdmin)
